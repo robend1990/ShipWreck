55eab002bbf8958640b50fdb3ddb97846c2a5b13
robend1990 <robend1990@vp.pl>
2017-04-13 19:14:31 +0200

    added spring boot web starter and first Hello world Homecontroller

--- src/main/java/com/shipwreck/App.java
+++ src/main/java/com/shipwreck/App.java
@@ -1,13 +1,14 @@
 package com.shipwreck;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Scan project for Spring components and autowires up spring libraries and enable auto configuration
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
